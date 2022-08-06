7c5ff2a04b0b003b019048bc29c90161dae7738e
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-08-06 04:31:37 +0100

    Update Tables.ts

--- Infrastructure/lib/Tables.ts
+++ Infrastructure/lib/Tables.ts
@@ -4,7 +4,7 @@ import { ENVIRONMENT, SERVICE } from "@infrastructure/common/configuration";
 
 export const buildProductsTable = (context: Construct): Table => {
     return new Table(context, "ProductsTable", <TableProps>{
-        tableName: `${ENVIRONMENT}-${SERVICE}-Table`,
+        tableName: `${ENVIRONMENT}-${SERVICE}Table`,
         billingMode: BillingMode.PAY_PER_REQUEST,
         partitionKey: { name: "id", type: AttributeType.STRING }
     });
