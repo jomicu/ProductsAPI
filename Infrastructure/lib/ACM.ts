ab3ee9884fa67c44c607f33b2f5bbbf0196c0d0f
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-08-07 01:03:38 +0100

    Updatee??

--- Infrastructure/lib/ACM.ts
+++ Infrastructure/lib/ACM.ts
@@ -1,10 +1,10 @@
 import { Construct } from "constructs";
-import { HostedZone } from "aws-cdk-lib/aws-route53";
+import { IHostedZone } from "aws-cdk-lib/aws-route53";
 import { Certificate, CertificateValidation } from "aws-cdk-lib/aws-certificatemanager";
 
 import { DOMAIN } from "@infrastructure/configuration";
 
-export const getJomicuCertificate = (context: Construct, hostedZone: HostedZone): Certificate => {
+export const getJomicuCertificate = (context: Construct, hostedZone: IHostedZone): Certificate => {
     return new Certificate(context, "Certificate", {
         domainName: DOMAIN,
         validation: CertificateValidation.fromDns(hostedZone),
--- Infrastructure/lib/Route53.ts
+++ Infrastructure/lib/Route53.ts
@@ -1,7 +1,7 @@
 import { Construct } from "constructs";
-import { HostedZone, HostedZoneAttributes } from "aws-cdk-lib/aws-route53";
+import { HostedZone, HostedZoneAttributes, IHostedZone } from "aws-cdk-lib/aws-route53";
 
-export const getJomicuRoute53 = (context: Construct) => { 
+export const getJomicuRoute53 = (context: Construct): IHostedZone => { 
     return HostedZone.fromHostedZoneAttributes(context, "HostedZone", <HostedZoneAttributes>{
         zoneName: "jomicu.com",
     });
