ee2afad92ec312d39e1824901310567f7f6b6680
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-08-08 01:48:58 +0100

    Update ACM.ts

--- Infrastructure/lib/ACM.ts
+++ Infrastructure/lib/ACM.ts
@@ -2,11 +2,11 @@ import { Construct } from "constructs";
 import { IHostedZone } from "aws-cdk-lib/aws-route53";
 import { Certificate, CertificateValidation } from "aws-cdk-lib/aws-certificatemanager";
 
-import { CERTIFICATE_DOMAIN_NAME } from "@infrastructure/configuration";
+import { SUBDOMAIN } from "@infrastructure/configuration";
 
 export const getJomicuCertificate = (context: Construct, hostedZone: IHostedZone): Certificate => {
     return new Certificate(context, "Certificate", {
-        domainName: CERTIFICATE_DOMAIN_NAME,
+        domainName: `${SUBDOMAIN}.${hostedZone.zoneName}`,
         validation: CertificateValidation.fromDns(hostedZone),
   });
 }
