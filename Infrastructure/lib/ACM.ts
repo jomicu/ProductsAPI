f0b91bf6a654761fb709944d73c64224c55c0884
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-08-08 01:55:22 +0100

    Revert "Update ACM.ts"
    This reverts commit ee2afad92ec312d39e1824901310567f7f6b6680.

--- Infrastructure/lib/ACM.ts
+++ Infrastructure/lib/ACM.ts
@@ -2,11 +2,11 @@ import { Construct } from "constructs";
 import { IHostedZone } from "aws-cdk-lib/aws-route53";
 import { Certificate, CertificateValidation } from "aws-cdk-lib/aws-certificatemanager";
 
-import { SUBDOMAIN } from "@infrastructure/configuration";
+import { CERTIFICATE_DOMAIN_NAME } from "@infrastructure/configuration";
 
 export const getJomicuCertificate = (context: Construct, hostedZone: IHostedZone): Certificate => {
     return new Certificate(context, "Certificate", {
-        domainName: `${SUBDOMAIN}.${hostedZone.zoneName}`,
+        domainName: CERTIFICATE_DOMAIN_NAME,
         validation: CertificateValidation.fromDns(hostedZone),
   });
 }
