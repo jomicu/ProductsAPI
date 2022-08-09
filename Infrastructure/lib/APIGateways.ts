f34ce877ac79303abab3d83c40dc1978951d2466
José Miguel Ribeiro Cunha <44200744+Migas99@users.noreply.github.com>
2022-08-09 19:54:17 +0100

    update

--- Infrastructure/lib/APIGateways.ts
+++ Infrastructure/lib/APIGateways.ts
@@ -2,7 +2,7 @@ import { Construct } from "constructs";
 import { Function } from "aws-cdk-lib/aws-lambda";
 import { IHostedZone } from "aws-cdk-lib/aws-route53";
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
-import { RestApi, RestApiProps, DomainNameOptions, LambdaIntegration, ApiKeySourceType, LambdaIntegrationOptions, MethodOptions, EndpointType, EndpointConfiguration, ModelOptions, PassthroughBehavior, ApiKeyOptions, ApiKey, ApiKeyProps, UsagePlan, UsagePlanProps, CorsOptions, ThrottleSettings } from "aws-cdk-lib/aws-apigateway";
+import { RestApi, RestApiProps, DomainNameOptions, LambdaIntegration, ApiKeySourceType, LambdaIntegrationOptions, MethodOptions, EndpointType, EndpointConfiguration, ModelOptions, PassthroughBehavior, ApiKeyOptions, ApiKey, ApiKeyProps, UsagePlan, UsagePlanProps, CorsOptions, ThrottleSettings, UsagePlanPerApiStage } from "aws-cdk-lib/aws-apigateway";
 import { CreateProductsRequestModel } from "@infrastructure/lib/JsonSchemas";
 import { CreateProductsRequestTemplate, CreateProductsResponsesTemplates } from "@infrastructure/lib/Templates";
 
@@ -39,16 +39,18 @@ export const buildProductsAPIGateway = (
             ],
             allowMethods: ["POST"],
             allowOrigins: ["*"]
-          }
+        }
     });
 
     const usagePlan = productsAPI.addUsagePlan("DefaultUsagePlan", <UsagePlanProps>{
-        name: "Easy",
-        throttle: <ThrottleSettings>{
-          rateLimit: 10,
-          burstLimit: 2
-        }
-      });
+        name: "DefaultUsagePlan",
+        apiStages: [
+            <UsagePlanPerApiStage>{
+                api: productsAPI,
+                stage: productsAPI.deploymentStage
+            }
+        ]
+    });
       
     const key = productsAPI.addApiKey("DefaultAPIKey", <ApiKeyOptions>{
         apiKeyName: `${ENVIRONMENT}-${SERVICE}API-key`
--- Infrastructure/lib/Tables.ts
+++ Infrastructure/lib/Tables.ts
@@ -1,11 +1,11 @@
 import { Construct } from "constructs";
-import { Table, TableProps, BillingMode, AttributeType } from "aws-cdk-lib/aws-dynamodb";
+import { Table, TableProps, BillingMode, AttributeType, Attribute } from "aws-cdk-lib/aws-dynamodb";
 import { ENVIRONMENT, SERVICE } from "@infrastructure/configuration";
 
 export const buildProductsTable = (context: Construct): Table => {
     return new Table(context, "ProductsTable", <TableProps>{
         tableName: `${ENVIRONMENT}-${SERVICE}-table`,
         billingMode: BillingMode.PAY_PER_REQUEST,
-        partitionKey: { name: "id", type: AttributeType.STRING }
+        partitionKey: <Attribute>{ name: "id", type: AttributeType.STRING }
     });
 }
